ef6a65940b9e2f02c1baf36a700c1dd6fc495fd4
mitchm23 <69411959+mitchm23@users.noreply.github.com>
2021-03-22 13:43:58 -0700

    fixes

--- server.js
+++ server.js
@@ -19,38 +19,37 @@ function run(){
         message: 'What would you like to do?',
         choices: ['View All Employees', 'View Roles', 'View Departments', 'Add Employee', 'Remove Employee', 'Add Department', 'Add Role', 'Remove Role', 'Remove Department', 'Quit']
     }).then(function(answer) {
-        console.log(answer);
             if(answer.questions === "View All Employees") {
                 viewAllEmployees();
             }
-            else if(answer.questions === 1){
+            else if(answer.questions === "View Roles"){
                 viewRoles();
             }
-            else if(answer.questions === 2){
+            else if(answer.questions === "View Departments"){
                 viewDepartments();
             }
-            else if(answer.questions === 3){
+            else if(answer.questions === "Add Employee"){
                 addEmployee();
             }
-            else if(answer.questions === 4){
+            else if(answer.questions === "Remove Employee"){
                 removeEmployee();
             }
-            else if(answer.questions === 5){
+            else if(answer.questions === "Add Department"){
                 addDepartment();
             }
-            else if(answer.questions === 6){
+            else if(answer.questions === "Add Role"){
                 addRole();
             }
-            else if(answer.questions === 7){
+            else if(answer.questions === "Remove Role"){
                 removeRole();
             }
-            else if(answer.questions === 8){
+            else if(answer.questions === "Remove Department"){
                 removeDepartment();
             }
-            else if(answer.questions === 9){
+            else if(answer.questions === "Quit"){
                 quit();
             }
-        
+        console.log(answer.questions);
     })
 }
 
@@ -74,27 +73,65 @@ function viewDepartments(){
     connection.query("SELECT * FROM department",(err,result)=> {
         if(err) throw err;
         console.table(result);
+        run();
     })
 }
-run();
 
-// function addEmployee(){
-//     connection.query("SELECT * FROM department",(err,result)=> {
-//         if(err) throw err;
-//         console.table(result);
-//     })
-// }
 
-// function addDepartment(){
-//     inquirer.prompt([
-//         {
-//             name: "department",
-//             type: "input",
-//             message: "What is the name of your department?"
-//         }
-//     ]).then((response)=>{
-//         connection.query("INSERT INTO department(name))", [response.department])
-//     })
+function addEmployee(){
+    inquirer.prompt([
+        {
+            name: "employee",
+            type: "input",
+            message: "What is the name of your employee?"
+        }
+    ])
+    connection.query("INSERT INTO employee(first_name,last_name, role_id, manager_id) VALUES (?, ?, ?, ?);", [response.first_name,],(err,result)=> {
+        if(err) throw err;
+        console.table(result);
+    })
+}
+
+function addDepartment(){
+    inquirer.prompt([
+        {
+            name: "department",
+            type: "input",
+            message: "What is the name of your department?"
+        }
+    ]).then((response)=>{
+        connection.query("INSERT INTO department(department_name) VALUES (?);", [response.department], (err,result)=> {
+            if(err) throw err;
+            console.log("Successfully added Department!");
+            run();
+        })
+    })
     
-//     }
+    }
 
+    function addRole(){
+        inquirer.prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the name of the role?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of the role?"
+            },
+            {
+                name: "department_id",
+                type: "input",
+                message: "What is the department id?"
+            }
+        ]).then((response)=>{
+            connection.query("INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?);"), [response.title, response.salary, response.department_id],(err,results)=> {
+                if(err) throw err;
+                console.table(results);
+            }
+        })
+    }
+
+run();
